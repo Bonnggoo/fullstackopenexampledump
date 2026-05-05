f146c197a44bce607fd690cd194918157e63f6d7
unknown <labuabbas@gmail.com>
2026-05-05 04:00:03 +0300

    Part4 b

--- part3/mongo.js
+++ part3/mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.mruf8aj.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://fullstack:${password}@cluster0.mruf8aj.mongodb.net/testNoteApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery',false)
 
@@ -21,7 +21,7 @@ const noteSchema = new mongoose.Schema({
 const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: 'Mongoose makes things easy',
+  content: 'HTML is easy',
   important: true,
 })
 
--- part3/utils/config.js
+++ part3/utils/config.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
-const config = require('./utils/config')
-const logger = require('./utils/logger')
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
 
-logger.info(`Server running on port ${config.PORT}`)
 
-module.exports = { MONGODB_URI, PORT }
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
+
+module.exports = {
+  MONGODB_URI,
+  PORT
+}
